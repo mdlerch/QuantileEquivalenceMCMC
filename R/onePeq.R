b57c4f37b0bb16e1ee0fb604ca1e76b575a73456
Michael Lerch <mdlerch@gmail.com>
2014-05-20 22:16:29 -0600

    Update onePeq documentation

--- R/onePeq.R
+++ R/onePeq.R
@@ -1,21 +1,34 @@
-#' Two one-sided test of equivalence for summary stats
+#' One parameter test of equivalence
 #'
 #' @description
-#' My description
+#' Perform a one parameter test of equivalence for a single sample based on the
+#' sample statistics
 #'
-#' @param mu description
-#' @param se description
-#' @param n description
-#' @param delta description
-#' @param alpha description
-#' @param H0 description
+#' @param mu mean of the sample
+#' @param sd true standard deviation
+#' @param n sample size
+#' @param epsilon margin of error
+#' @param alpha significance level
+#' @param null null hypothesis value
 #'
 #' @details
-#' This is a
+#' Consider X_i ~ (theta, sigma) with sigma known.  We test the hypothesis H:
+#' abs(theta - null) >= epsilon against K: abs(theta - null) < epsilon.  From
+#' Wellek 2010 4.1, the UMP test is to compare abs(mu) to n^(-1/2) * C where C
+#' is the alphath quantile from a chisquare distribution with non centrality
+#' prameter sqrt(n)*epsilon and one degree of freedom.
 #'
+#' Wellek shows this for X_i ~ N(theta, sigma), but I think it should follow as
+#' long as \overline(X_i) ~ N(theta, sigma).
+#'
+#' @references
+#' Wellek 2010 Testing Statistical Hypotheses of Equivalence and Noninferiority
+#'
+#'
+#' @export
 #' @return 1 for reject, 0 for FTR
 #'
-onePeq <- function(mu, sd, n, epsilon, alpha = 0.05, null = 0)
+onePeq <- function(mu, sd = 1, n, epsilon, alpha = 0.05, null = 0)
 {
     mu <- (mu - null) / sd
     eps_twid <- sqrt(n) * epsilon
