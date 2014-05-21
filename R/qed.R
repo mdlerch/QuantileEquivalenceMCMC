90d402caaa5e5fc08850ff468f83fa2020106237
Michael Lerch <mdlerch@gmail.com>
2014-05-21 13:31:23 -0600

    Make qed accept matrix of chains

--- R/qed.R
+++ R/qed.R
@@ -3,23 +3,29 @@
 #' @description
 #' Perform the quantile equivalence diagnostic for a single chain
 #'
-#' @param X chain
+#' @param X matrix of chains
 #' @param q quantile of interest
 #' @param quant percentile of quantile
 #' @param epsilon margin of error on quantile
 #' @param alpha significance level
 #'
 #' @details
-#' work in progress
+#' Argument X is a matrix where each column is a chain
 #'
 #' @return returned
 #' @export
 #'
 qed <- function(X, q, quant, epsilon = 0.01, alpha = 0.05)
 {
-    # find the chain percentile
-    p <- sum(X > q) / length(X)
-    # use q for standard deviation
-    s <- sqrt(q * (1 - q))
-    onePeq(p - q, s, length(X), epsilon, alpha)
+    nchains <- ncol(X)
+    result <- numeric(nchains)
+    for (i in 1:nchains)
+    {
+        # find the chain percentile
+        p <- sum(X > q) / nrow(X)
+        # use q for standard deviation
+        s <- sqrt(q * (1 - q))
+        result[i] <- onePeq(p - q, s, nrow(X), epsilon, alpha)
+    }
+    return(prod(result))
 }
