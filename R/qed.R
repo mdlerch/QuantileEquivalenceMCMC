da6cf167201b1eb1149db8a3ce57af8e123f1aac
Michael Lerch <mdlerch@gmail.com>
2014-05-21 18:51:44 -0600

    Fix some mistakes
    Had some serious mistakes in qed prep to send to onePeq

--- R/qed.R
+++ R/qed.R
@@ -23,13 +23,15 @@ qed <- function(X, quant, prob, epsilon = 0.01, alpha = 0.05)
     {
         # find the chain probability of less than quant
         # this is our uncentered Xbar
-        p <- sum(X < quant) / nrow(X)
+        p <- sum(X[ , i] < quant) / nrow(X)
         # use prob for standard deviation
         s <- sqrt(prob * (1 - prob))
         # standardized and centralized Xbar
         # p ~ N(prob, sqrt(prob * (1 - prob) / n))
         # Xbar ~ N(0, 1 / sqrt(n))
         Xbar <- (p - prob) / s
+        # epsilon in the transformed space
+        epsilon <- epsilon / s
         # do test on each chain
         result[i] <- onePeq(Xbar, 1, nrow(X), epsilon, alpha)
     }
