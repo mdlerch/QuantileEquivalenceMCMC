148d4d0478187d9217f100a8b4d0b549532130a3
Michael Lerch <mdlerch@gmail.com>
2014-05-21 18:16:47 -0600

    Improve variable names.
    Quant is a value (a realization of a random variable) and prob is a
    probability (that is associated with values less than quant)

--- R/qed.R
+++ R/qed.R
@@ -4,8 +4,8 @@
 #' Perform the quantile equivalence diagnostic for a single chain
 #'
 #' @param X matrix of chains
-#' @param q quantile of interest
-#' @param quant percentile of quantile
+#' @param quant quantile of interest
+#' @param prob null probability of observation below quantile quant
 #' @param epsilon margin of error on quantile
 #' @param alpha significance level
 #'
@@ -15,18 +15,21 @@
 #' @return returned
 #' @export
 #'
-qed <- function(X, q, quant, epsilon = 0.01, alpha = 0.05)
+qed <- function(X, quant, prob, epsilon = 0.01, alpha = 0.05)
 {
     nchains <- ncol(X)
     result <- numeric(nchains)
     for (i in 1:nchains)
     {
-        # find the chain percentile
-        p <- sum(X > q) / nrow(X)
-        # use q for standard deviation
-        s <- sqrt(q * (1 - q))
+        # find the chain probability of less than quant
+        # this is our uncentered Xbar
+        p <- sum(X < quant) / nrow(X)
+        # use prob for standard deviation
+        s <- sqrt(prob * (1 - prob))
         # standardized and centralized Xbar
-        Xbar <- (p - q) / s
+        # p ~ N(prob, sqrt(prob * (1 - prob) / n))
+        # Xbar ~ N(0, 1 / sqrt(n))
+        Xbar <- (p - prob) / s
         # do test on each chain
         result[i] <- onePeq(Xbar, 1, nrow(X), epsilon, alpha)
     }
