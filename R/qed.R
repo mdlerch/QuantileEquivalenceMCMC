dbde526c4b948c8258521fa79929a2173f41bc05
Michael Lerch <mdlerch@gmail.com>
2014-05-28 15:28:07 -0600

    Fix error; move calculation.
    Move calculation that only needs to be done once out of for loop.  Fix
    mistake that epsilon was being changed for each chain.

--- R/qed.R
+++ R/qed.R
@@ -18,10 +18,10 @@ qed <- function(X, prob, epsilon = 0.005, alpha = 0.05)
 {
     nchains <- ncol(X)
     result <- numeric(nchains)
+    # find the overall quantile
+    quant <- quantile(X, prob)
     for (i in 1:nchains)
     {
-        # find the overall quantile
-        quant <- quantile(X, prob)
         # find the chain probability of less than quant
         # this is our uncentered Xbar
         p <- sum(X[ , i] < quant) / nrow(X)
@@ -32,9 +32,9 @@ qed <- function(X, prob, epsilon = 0.005, alpha = 0.05)
         # Xbar ~ N(0, 1 / sqrt(n))
         Xbar <- (p - prob) / s
         # epsilon in the transformed space
-        epsilon <- epsilon / s
+        epsilon.test <- epsilon / s
         # do test on each chain
-        result[i] <- onePeq(Xbar, 1, nrow(X), epsilon, alpha)
+        result[i] <- onePeq(Xbar, 1, nrow(X), epsilon.test, alpha)
     }
     return(prod(result))
 }
