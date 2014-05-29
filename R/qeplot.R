fc5396aa9c6a9d0bad2a577c824b864e619e006f
Michael Lerch <mdlerch@gmail.com>
2014-05-28 20:46:01 -0600

    Get basic plot done

--- R/qeplot.R
+++ R/qeplot.R
@@ -17,6 +17,8 @@
 #'
 qeplot <- function(chains, prob, ...)
 {
+
+    # get probabilities associated with overall quantile
     quant <- quantile(chains, prob)
     p <- matrix(NA, ncol = ncol(chains), nrow = length(prob))
     for (i in 1:length(prob))
@@ -24,15 +26,25 @@ qeplot <- function(chains, prob, ...)
         p[i, ] <- apply(chains, 2, function(x) sum(x < quant[i]) / length(x))
     }
 
+    # get quantiles associated with specified probability
     q <- matrix(NA, ncol = ncol(chains), nrow = length(prob))
     for (i in 1:length(prob))
     {
         q[i, ] <- apply(chains, 2, function(x) quantile(x, prob[i]))
     }
 
-    plot(p ~ q, type = "n")
+    plot(p ~ q, type = "n", ...)
 
-    plot(prob ~ quant, type = "p")
+    points(prob ~ quant, pch = 0)
+    for (i in 1:length(prob))
+    {
+        for (j in 1:ncol(chains))
+        {
+            lines(c(quant[i], q[i, j]), c(p[i, j], prob[i]), col = j)
+            points(quant[i], p[i, j], col = j, pch = 19)
+            points(q[i, j], prob[i], col = j, pch = 19)
+        }
+    }
 
     return(list(prob = prob, quant = quant, p = p, q = q))
 }
