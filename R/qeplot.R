a1df40c8b7a8532844f8d790bbe6f58c512746e1
Michael Lerch <mdlerch@gmail.com>
2014-05-28 19:53:04 -0600

    Start working on plotting function

---
+++ R/qeplot.R
@@ -0,0 +1,24 @@
+#' Plot quantiles from each chain
+#'
+#' @description
+#' Plot quantiles from each chain
+#'
+#' @param chains A matrix of MCMC's.  Each chain is a column.
+#' @param p A probability for a quantile or a vector of probabilities.
+#'
+#' @details
+#' Plots specified quantiles from each chain so that the researcher can
+#' assess convergence
+#'
+#' @return Nothing is returned
+#' @export
+#'
+qeplot <- function(chains, p)
+{
+    quant <- quantile(chains, p)
+    for (i in 1:length(p))
+    {
+        q <- apply(chains, 2, function(x) sum(x > quant[i]) / length(x))
+    }
+    return(q)
+}
