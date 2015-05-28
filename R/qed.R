57bb65abedb161028d4ad409b6b5396e1e7ab3d6
Michael Lerch <mdlerch@gmail.com>
2015-05-28 10:24:15 -0600

    Use chains not X, update qeplot doc

--- R/qed.R
+++ R/qed.R
@@ -1,11 +1,11 @@
-qed <- function(X, prob = NULL, quant = NULL, epsilon = 0.01, alpha = 0.05)
+qed <- function(chains, prob, quant, epsilon = 0.01, alpha = 0.05)
 {
     # check input
-    if (is.null(prob) & is.null(quant))
+    if (missing(prob) & missing(quant))
     {
         stop("Must provide one of prob or quant")
     }
-    else if (!is.null(prob) & !is.null(quant))
+    else if (!missing(prob) & !missing(quant))
     {
         stop("Provide only one of prob or quant")
     }
@@ -13,30 +13,30 @@ qed <- function(X, prob = NULL, quant = NULL, epsilon = 0.01, alpha = 0.05)
     {
         stop("alpha must be between 0 and 1")
     }
-    if (!is.null(prob) & (prob >= 1 | prob <= 0))
+    if (!missing(prob) & (prob >= 1 | prob <= 0))
     {
         stop("prob must be between 0 and 1")
     }
 
 
     # _quantile_ or _probability_ problem?
-    if (is.null(prob))
+    if (missing(prob))
     {
-        prob <- sum(X < quant) / length(X)
+        prob <- sum(chains < quant) / length(chains)
     }
 
-    nchains <- ncol(X)
-    n <- nrow(X)
+    nchains <- ncol(chains)
+    n <- nrow(chains)
     result <- numeric(nchains)
     # find the overall quantile
-    quant <- quantile(X, prob)
+    quant <- quantile(chains, prob)
     for (i in 1:nchains)
     {
-        # X ~ bernoulli(p)
+        # chains ~ bernoulli(p)
         # phat ~ N(p, sqrt(p (1 - p) / n))
-        phat <- sum(X[ , i] < quant) / nrow(X)
+        phat <- sum(chains[ , i] < quant) / nrow(chains)
         # use prob for standard deviation
-        # this is sd of X_i
+        # this is sd of chains_i
         s <- sqrt(prob * (1 - prob) / n)
         # standardized and centralized Z
         z <- 1 / s * (phat - prob)
